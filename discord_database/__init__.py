e89792a1bc227f93552370b1230353ec37cc54a0
cutebear <72639017+cutebear0123@users.noreply.github.com>
2021-08-07 19:01:31 -0700

    Create __init__.py

---
+++ discord_database/__init__.py
@@ -0,0 +1,18 @@
+import json
+def help():
+    helps='''
+    command:
+    locals.new :new user,arguments:ctx
+    globals.new :new user in all globsl,arguments:ctx
+    locals.load:load user data,arguments:ctx,item
+    globals.load:load user data in global,arguments:ctx,item
+    locals.save:save user data,arguments:ctx,key,item
+    globals.save:save user data in global,arguments:ctx,key,item
+    locals.plus:plus a number to a key,argquments:ctx,key,item(float)
+    globals.plus:plus a number to a key in global,argquments:ctx,key,item(float)
+    
+    '''
+    print(helps)
+    return helps
+from .globals import globals
+from .locals import locals
